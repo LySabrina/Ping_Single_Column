71f350a3fab8ccbf45dd031ef2c7c877974530e8
lysabrina <lysabrinak@gmail.com>
2024-01-12 12:57:17 -0800

    First commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const form = document.querySelector(".email-form");
+
+const input = form.querySelector(".email-form__email");
+
+const button = form.querySelector(".email-form__btn");
+
+/**
+ *
+ * @param {String} email Email that needs testing validation
+ * @returns boolean true or false if it's valid
+ */
+const emailValid = (email) => {
+  const regex = /[A-Za-z][A-Za-z0-9][A-Za-z0-9]+@[A-Za-z]+\.(com|org)/;
+  return regex.test(email);
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const error_message = document.querySelector(".error");
+  if (!emailValid(input.value)) {
+    // const error_message = document.createElement("p");
+    // error_message.textContent = "Please provide a valid email address";
+    // error_message.classList.add("error");
+    // form.insertAdjacentElement("afterend",error_message);
+
+    error_message.classList.remove("hide");
+  } else {
+    error_message.classList.add("hide");
+    alert("Sucess sign up");
+  }
+});
